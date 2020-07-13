7f5422e9e476145a6f2db35e4849be29ae33dc9c
nvkex <scrbwrm@gmail.com>
2020-07-13 16:52:11 +0530

    fix:insecure endpoint error

--- src/App.vue
+++ src/App.vue
@@ -26,14 +26,14 @@ export default {
   },
   methods:{
     deleteTodo(id){
-      axios.delete(`http://jsonplaceholder.typicode.com/todos/${id}`)
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
         .then(() => this.todos = this.todos.filter( todo => todo.id !== id))
         .catch(err => console.log(err))
       
     },
     addTodo(newTodo){
       const {title, completed} = newTodo;
-      axios.post('http://jsonplaceholder.typicode.com/todos', {title, completed})
+      axios.post('https://jsonplaceholder.typicode.com/todos', {title, completed})
         .then(res => this.todos = [...this.todos, res.data])
         .catch(err => console.log(err))
       
@@ -41,7 +41,7 @@ export default {
   },
 
   created(){
-    axios.get('http://jsonplaceholder.typicode.com/todos?_limit=10')
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
       .then(res => {
         this.todos = res.data;
       })
